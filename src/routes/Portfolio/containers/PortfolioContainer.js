4033b9c59a1f43c5d9c50ca8eff8c82d134de361
shizofrennik <Serge553590@mail.ru>
2017-08-04 11:19:13 +0600

    modal submit portfolio

--- src/routes/Portfolio/containers/PortfolioContainer.js
+++ src/routes/Portfolio/containers/PortfolioContainer.js
@@ -7,17 +7,29 @@ import HTML5Backend, {NativeTypes} from 'react-dnd-html5-backend';
 import TargetBox from '../components/TargetBox';
 import GalleryItem from 'components/GalleryItem';
 import Notification from 'components/Notification';
+import { Modal } from 'react-bootstrap';
 
 class PortfolioContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      showModal: false,
       droppedFiles: []
     };
 
     this.handleFileDrop = this.handleFileDrop.bind(this);
     this.setDropedFiles = this.setDropedFiles.bind(this);
     this.deleteFile = this.deleteFile.bind(this);
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  openModal() {
+    this.setState({showModal: true});
+  }
+
+  closeModal() {
+    this.setState({showModal: false});
   }
 
   deleteFile(file) {
@@ -65,7 +77,10 @@ class PortfolioContainer extends React.Component {
         &nbsp; to get started.
       </span>) :
       (<span>
-        <a className="diokan-notification__link" href="#">Submit your portfolio</a> to get started.
+        <a
+          className="diokan-notification__link"
+          onClick={this.openModal}
+          href="#">Submit your portfolio</a> to get started.
       </span>);
     return (
       <Notification>
@@ -98,6 +113,27 @@ class PortfolioContainer extends React.Component {
             {this.getImages(droppedFiles)}
           </div>
         </div>
+        
+        <Modal
+          show={this.state.showModal}
+          onHide={this.closeModal}
+          className="diokan-form-action diokan-form-action__submit">
+          <Modal.Header className="diokan-form-action__header">
+            <Modal.Title className="diokan-title-primary_sub">Submit Portfolio</Modal.Title>
+            <button onClick={this.closeModal} className="diokan-btn diokan-btn-close-icon" />
+          </Modal.Header>
+          <Modal.Body className="diokan-form-action__body">
+            <div className="diokan-form-action__description">
+              You’ve uploaded 10 photos.<br />
+              Please submit your portfolio so your account can be approved.
+            </div>
+            <div className="diokan-form__description">
+              It will only take us 1-2 business days to review,
+              and then you can start earning money.
+            </div>
+            <button className="diokan-btn diokan-btn-primary__big">Submit Portfolio</button>
+          </Modal.Body>
+        </Modal>
       </main>
     )
   }
